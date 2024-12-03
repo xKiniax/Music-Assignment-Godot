524b241ac77831161a285ebe3c353aaee7b0dfb4
xKiniax <erin.rogalska@gmail.com>
2024-12-03 00:32:22 +0000

    All 3 arrows created
    Need to do a few levels and menu screens and add/sync music :)

---
+++ music-game-project/scripts/Idle_ArrowRight.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+@onready var animp = $AnimationPlayer
+var sensor = 0
+
+
+func _process(delta: float) -> void:
+	
+	# Sensor and pressed "Good"
+	if sensor == 1:
+		if Input.is_action_just_pressed("ui_right"):
+			animp.play("Good")
+	
+	# Sensor and pressed "Bad"
+	if sensor == 0:
+		if Input.is_action_just_pressed("ui_right"):
+			
+			animp.play("Bad")
+
+func _on_Idle_ArrowRight_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	sensor = 1
+	Global.sensor_AL = 1
+	
+
+
+
+func _on_Idle_ArrowRight_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	sensor = 0
+	Global.sensor_AL = 0
---
+++ music-game-project/scripts/Move_ArrowRight.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+var speed = 100
+var sensor = 0
+
+func _process(delta):
+	
+	# Move
+	position.y -= speed * delta
+	
+	# Screen exit
+	if position.y < -50:
+		queue_free()
+	
+	# Sesnor and pressed
+	if sensor == 1:
+		# Add "Global" > new
+		if Global.sensor_AL == 1:
+			if Input.is_action_just_pressed("ui_right"):
+				queue_free()
+	
+func _on_Move_ArrowRight_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	sensor = 1
+	
+func _on_Move_ArrowRight_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	sensor = 0
